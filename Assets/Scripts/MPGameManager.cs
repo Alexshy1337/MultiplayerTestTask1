6cafd060af861cb174e02de5eb18cf28d8e455ec
Alexshy <shyharrypotter@gmail.com>
2023-08-14 17:28:54 +0300

    to record current progress

--- Assets/Scripts/MPGameManager.cs
+++ Assets/Scripts/MPGameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,47 +13,80 @@ using UnityEngine;
 
 public class MPGameManager : NetworkBehaviour
 {
+
     public static MPGameManager instance { get; private set; }
+
+    private event EventHandler OnClientConnect;
+    private event EventHandler OnClientDisconnect;
+    private List<PlayerData> _players;
 
     private void Awake()
     {
-        instance= this;
-
+        instance = this;
         StartGame();
     }
 
-    private async void StartGame()
+
+    //NetworkManager.SpawnManager
+
+    //NetworkManager.OnClientDisconnectCallback
+
+    //NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnectedCallback;
+    //NetworkManager.Singleton.OnClientDisconnectCallback += OnClientDisconnectCallback;
+
+
+    private void StartGame()
     {
         if (LobbyManager.Instance.IsLobbyHost())
         {
             CreateRelay(LobbyManager.Instance.GetJoinedLobby());
-            
+            NetworkManager.Singleton.StartHost();//returns bool, so should be used for actions on failure
         }
         else
         {
-            JoinAllocation joinAllocation = await JoinRelay(LobbyManager.Instance.GetJoinedLobby().Data[LobbyManager.KEY_RELAY_JOIN_CODE].Value);
-            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
-            NetworkManager.Singleton.StartClient();
+            JoinRelay(LobbyManager.Instance.GetJoinedLobby().Data[LobbyManager.KEY_RELAY_JOIN_CODE].Value);
+            NetworkManager.Singleton.StartClient();//returns bool, so should be used for actions on failure
         }
 
     }
 
+    public void RegisterPlayerServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        if (IsHost)
+        {
+            var clientId = serverRpcParams.Receive.SenderClientId;
+            if (NetworkManager.ConnectedClients.ContainsKey(clientId))
+            {
+                _players.Add(new PlayerData() { Id = clientId, Health = 100, CoinsCollected = 0, Name = ""}) ;
+            }
+        }
+
+
+        //SSTools.ShowMessage("lobby player id = " + LobbyManager.Instance.GetJoinedLobby(), SSTools.Position.bottom, SSTools.Time.twoSecond);
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        RegisterPlayerServerRpc(); // serverRpcParams will be filled in automatically
+    }
+
     private async void CreateRelay(Lobby joinedLobby)
     {
-
         Allocation allocation = await AllocateRelay();
+        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
+        UpdateLobbyWithRelayCode(joinedLobby.Id, allocation);
+    }
 
+    private async void UpdateLobbyWithRelayCode(string lobbyId, Allocation allocation)
+    {
         string relayJoinCode = await GetRelayJoinCode(allocation);
 
-        await LobbyService.Instance.UpdateLobbyAsync(joinedLobby.Id, new UpdateLobbyOptions
+        await LobbyService.Instance.UpdateLobbyAsync(lobbyId, new UpdateLobbyOptions
         {
             Data = new Dictionary<string, DataObject> {
                      { LobbyManager.KEY_RELAY_JOIN_CODE , new DataObject(DataObject.VisibilityOptions.Member, relayJoinCode) }
-                 }
+            }
         });
-
-        NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(allocation, "dtls"));
-        NetworkManager.Singleton.StartHost();
     }
 
     private async Task<Allocation> AllocateRelay()
@@ -86,17 +120,24 @@ public class MPGameManager : NetworkBehaviour
         }
     }
 
-    private async Task<JoinAllocation> JoinRelay(string joinCode)
+    private async void JoinRelay(string joinCode)
     {
         try
         {
             JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
-            return joinAllocation;
+            NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(new RelayServerData(joinAllocation, "dtls"));
         }
         catch (RelayServiceException e)
         {
             Debug.Log(e);
-            return default;
         }
+    }
+
+    public struct PlayerData
+    {
+        public UInt64 Id; // NetworkManager -> ClientConnectionId
+        public string Name;
+        public short Health;
+        public short CoinsCollected;
     }
 }
